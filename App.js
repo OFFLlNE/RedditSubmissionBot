69ebeb2eadeed141c3a63a7a3832947e40139efd
OFFLlNE <valjalauri@gmail.com>
2018-08-22 23:18:03 +0300

    Add finalized bot code

---
+++ App.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+const conf = new Snoowrap({
+  userAgent: 'reddit-bot-example-node',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(conf);
+
+const streamOptions = {
+    subreddit: 'testingground4bots',
+    results: 25
+};
+
+const comments = client.CommentStream(streamOptions);
+
+// On comment, perform whatever logic you want to do
+comments.on('comment', (comment) => {
+    console.log(comment);
+});
